a372e1932bab52bb9242d25ffb9253a580fc70c9
Jason Duffy <jasonsduffy@gmail.com>
2013-07-20 22:19:28 -0400

    add use strict

--- javascript/rna-transcription/dna.js
+++ javascript/rna-transcription/dna.js
@@ -1,11 +1,11 @@
-// ok, now that I see a CoffeeScript track is on the way, I'll stop
+(function() {
+  "use strict";
 
-DNA = function(sequence) {
-  this.sequence = sequence;
-};
+  global.DNA = function(sequence) {
+    this.sequence = sequence;
+  };
 
-DNA.prototype.toRNA = function() {
-  return this.sequence.replace(/T/g, 'U');
-};
-
-global.DNA = DNA;
+  DNA.prototype.toRNA = function() {
+    return this.sequence.replace(/T/g, 'U');
+  };
+})();
