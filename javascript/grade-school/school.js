4de25bceefd03ef95300c825494e9674590baee1
Jason Duffy <jasonsduffy@gmail.com>
2013-07-24 13:26:55 -0400

    js/grade-school don't overwrite prototype

---
+++ javascript/grade-school/school.js
@@ -0,0 +1,32 @@
+(function() {
+  "use strict";
+
+  global.School = function(name) {
+    this.db = {};
+  };
+
+  School.prototype.add = function(name, grade) {
+    if (this.db.hasOwnProperty(grade)) {
+      this.db[grade].push(name);
+    } else {
+      this.db[grade] = [ name ];
+    }
+  };
+
+  School.prototype.grade = function(grade) {
+    if (this.db.hasOwnProperty(grade)) {
+      return this.db[grade];
+    } else {
+      return [];
+    }
+  };
+
+  School.prototype.sort = function() {
+    var result = {};
+    var grades = Object.getOwnPropertyNames(this.db);
+    grades.sort().forEach( function(grade, i, a) {
+      result[grade] = this[grade].sort();
+    }, this.db)
+    return result;
+  };
+})();
