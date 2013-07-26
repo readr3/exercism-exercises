3120c3d5d4821da64ca5851340e2eeffb5cb3638
Jason Duffy <jasonsduffy@gmail.com>
2013-07-26 07:29:19 -0400

    js/bob refactor

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,26 +1,32 @@
-Bob = function() {
-  this.hey = function(phrase) {
-    switch (true) {
-      case this.isSilent(phrase):
-        return 'Fine, be that way.';
-      case this.isShouting(phrase):
-        return 'Woah, chill out!';
-      case this.isAskingAQuestion(phrase):
-        return 'Sure';
-      default:
-        return 'Whatever';
+(function() {
+  "use strict";
+
+  function Bob() {
+    function isSilent(phrase) {
+      return phrase === '';
     }
-  };
 
-  this.isSilent = function(phrase) {
-    return phrase === '';
-  };
+    function isShouting(phrase) {
+      return phrase.toUpperCase() === phrase;
+    }
 
-  this.isShouting = function(phrase) {
-    return phrase.toUpperCase() === phrase;
-  };
+    function isAskingAQuestion(phrase) {
+      return phrase[phrase.length - 1] === '?';
+    }
 
-  this.isAskingAQuestion = function(phrase) {
-    return (/\?$/).test(phrase);
-  };
-};
+    this.hey = function(phrase) {
+      switch (true) {
+        case isSilent(phrase):
+          return 'Fine, be that way.';
+        case isShouting(phrase):
+          return 'Woah, chill out!';
+        case isAskingAQuestion(phrase):
+          return 'Sure';
+        default:
+          return 'Whatever';
+      }
+    };
+  }
+
+  module.exports = Bob;
+})();
