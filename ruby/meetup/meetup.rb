cd66bfc18c658aeb3bee5261a74706eae7189d16
Jason Duffy <jasonsduffy@gmail.com>
2013-07-27 23:32:26 -0400

    rb/meetup nit fix

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -9,7 +9,7 @@ class Meetup
     define_method("#{prefix}teenth") do
       (13..19).each do |n|
         d = Date.new(@year, @month, n)
-        return d if isDay?(d, prefix)
+        return d if is_day?(d, prefix)
       end
     end
 
@@ -17,7 +17,7 @@ class Meetup
       define_method("#{week}_#{prefix}day") do
         (1..7).each do |n|
           d = Date.new(@year, @month, n + i * 7)
-          return d if isDay?(d, prefix)
+          return d if is_day?(d, prefix)
         end
       end
     end
@@ -25,14 +25,14 @@ class Meetup
     define_method("last_#{prefix}day") do
       (@last_day-6..@last_day).each do |n|
         d = Date.new(@year, @month, n)
-        return d if isDay?(d, prefix)
+        return d if is_day?(d, prefix)
       end
     end
   end
 
   private
 
-  def isDay?(date, prefix)
+  def is_day?(date, prefix)
     date.send((prefix + 'day?').to_sym)
   end
 end
