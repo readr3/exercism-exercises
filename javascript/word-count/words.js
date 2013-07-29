dea05357a55d7d77a0fff07ef2c1fc06303f8c8f
Jason Duffy <jasonsduffy@gmail.com>
2013-07-29 10:20:07 -0400

    add alice

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,24 +1,51 @@
-global.Words = function(phrase) {
-  var wordList = phrase.toLowerCase().split(/\W+/);
+// global.Words = function(phrase) {
+//   var wordList = phrase.toLowerCase().split(/\W+/);
 
-  function WordCounts(wordList) {
-    var that = this;
+//   function WordCounts(wordList) {
+//     var that = this;
 
-    function increment(word) {
-      if (!that.hasOwnProperty(word))
-      {
-        that[word] = 0;
-      }
-      that[word] += 1;
-    }
+//     function increment(word) {
+//       if (!that.hasOwnProperty(word))
+//       {
+//         that[word] = 0;
+//       }
+//       that[word] += 1;
+//     }
 
-    wordList.forEach(function (word) {
-      if (word.length > 0)
-      {
-        increment(word);
-      }
-    });
+//     wordList.forEach(function (word) {
+//       if (word.length > 0)
+//       {
+//         increment(word);
+//       }
+//     });
+//   }
+
+//   this.count = new WordCounts(wordList);
+// };
+
+(function() {
+
+  var cleanMessage = function(message) {
+    return message.replace(/[|&;$%@"<>()+,:!!^]/g, "").toLowerCase();
   }
 
-  this.count = new WordCounts(wordList);
-};
+  var calculateWords = function(message) {
+    var out = {}
+      , arrayOfWords = cleanMessage(message).split(/\s+/);
+
+    for(i = 0; i < arrayOfWords.length; i++) {
+      var word = arrayOfWords[i];
+      if (out[word])
+        out[word] += 1;
+      else
+        out[word] = 1;
+    }
+
+    return out ;
+  }
+
+  this.Words = function(message) {
+    this.count = calculateWords(message);
+  };
+
+})();
