43fcf78f4d0dcb6ac0b35b07960b42ff31204a74
Jason Duffy <jasonsduffy@gmail.com>
2013-08-05 08:06:57 -0400

    js/leap

--- javascript/leap/year.js
+++ javascript/leap/year.js
@@ -1,17 +1,15 @@
-(function() {
-  'use strict';
+'use strict';
 
-  function Year(number) {
-    this.year = number;
-  }
+function Year(number) {
+  this.year = number;
+}
 
-  Year.prototype.isDivisibleBy = function(divisor) {
-    return this.year % divisor === 0;
-  };
+Year.prototype.isDivisibleBy = function(divisor) {
+  return this.year % divisor === 0;
+};
 
-  Year.prototype.isLeapYear = function() {
-    return (this.isDivisibleBy(4) && !this.isDivisibleBy(100)) || this.isDivisibleBy(400);
-  };
+Year.prototype.isLeapYear = function() {
+  return (this.isDivisibleBy(4) && !this.isDivisibleBy(100)) || this.isDivisibleBy(400);
+};
 
-  module.exports = Year;
-})();
+module.exports = Year;
