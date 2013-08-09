eaf6b6bf37b59ae37b5db63a9d0454d4aa4b2098
Jason Duffy <jasonsduffy@gmail.com>
2013-08-09 07:56:35 -0400

    rb/meetup use barewords

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,14 +1,16 @@
 class Meetup
+  attr_reader :year, :month, :last_day
+
   def initialize(month, year)
     @year = year
     @month = month
-    @last_day = Date.new(@year, @month, -1).day
+    @last_day = Date.new(year, month, -1).day
   end
 
   ['mon', 'tues', 'wednes', 'thurs', 'fri', 'satur', 'sun'].each do |prefix|
     define_method("#{prefix}teenth") do
       (13..19).each do |n|
-        d = Date.new(@year, @month, n)
+        d = Date.new(year, month, n)
         return d if is_day?(d, prefix)
       end
     end
@@ -16,15 +18,15 @@ class Meetup
     ['first', 'second', 'third', 'fourth'].each_with_index do |week, i|
       define_method("#{week}_#{prefix}day") do
         (1..7).each do |n|
-          d = Date.new(@year, @month, n + i * 7)
+          d = Date.new(year, month, n + i * 7)
           return d if is_day?(d, prefix)
         end
       end
     end
 
     define_method("last_#{prefix}day") do
-      (@last_day-6..@last_day).each do |n|
-        d = Date.new(@year, @month, n)
+      (last_day-6..last_day).each do |n|
+        d = Date.new(year, month, n)
         return d if is_day?(d, prefix)
       end
     end
