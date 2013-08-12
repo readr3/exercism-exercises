a505aa7fbcc7d1955e58eab67e672a20988f2bfe
Jason Duffy <jasonsduffy@gmail.com>
2013-08-11 21:20:25 -0400

    go back to my js/word-count

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,56 +1,24 @@
-// global.Words = function(phrase) {
-//   var wordList = phrase.toLowerCase().split(/\W+/);
+global.Words = function(phrase) {
+  var wordList = phrase.toLowerCase().split(/\W+/);
 
-//   function WordCounts(wordList) {
-//     var that = this;
+  function WordCounts(wordList) {
+    var that = this;
 
-//     function increment(word) {
-//       if (!that.hasOwnProperty(word))
-//       {
-//         that[word] = 0;
-//       }
-//       that[word] += 1;
-//     }
+    function increment(word) {
+      if (!that.hasOwnProperty(word))
+      {
+        that[word] = 0;
+      }
+      that[word] += 1;
+    }
 
-//     wordList.forEach(function (word) {
-//       if (word.length > 0)
-//       {
-//         increment(word);
-//       }
-//     });
-//   }
-
-//   this.count = new WordCounts(wordList);
-// };
-
-function Words(words) {
-  'use strict';
-
-  var counts, i, splitWords;
-
-  counts = {};
-  splitWords = words.split(' ');
-
-  function toAlphaChars(string) {
-    return string.replace(/[^\w]+/, '', '$&');
+    wordList.forEach(function (word) {
+      if (word.length > 0)
+      {
+        increment(word);
+      }
+    });
   }
 
-  for (i in splitWords) {
-    var word = toAlphaChars(splitWords[i]).toLowerCase();
-
-    if (word.length === 0) {
-      continue;
-    }
-
-    if (counts.hasOwnProperty(word)) {
-      counts[word]++;
-    }
-    else {
-      counts[word] = 1;
-    }
-  }
-
-  this.count = counts;
-}
-
-module.exports = Words;
+  this.count = new WordCounts(wordList);
+};
