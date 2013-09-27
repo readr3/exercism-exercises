a9e0367ec46f0ef5295a99a60329ff02aae11305
Jason Duffy <jasonsduffy@gmail.com>
2013-09-27 08:21:50 -0400

    rb/sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,8 +1,28 @@
 class Sieve
-  def initialize(up_to)
+  attr_reader :primes
+
+  def initialize(n)
+    @primes = sieve(n)
   end
 
-  def primes
-    return [2, 3, 5, 7]
+  private
+
+  def divisible(numerator, denominator)
+    numerator % denominator == 0
+  end
+
+  def sieve(n)
+    possibilities = (2..n).to_a
+    primes = []
+
+    while possibilities.length > 0 do
+      prime = possibilities.shift
+      primes << prime
+      possibilities = possibilities.select { |possibility|
+        !divisible(possibility, prime)
+      }
+    end
+
+    primes
   end
 end
