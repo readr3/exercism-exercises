08e03c8ef993f6982582b388bae9576fb6007da2
Jason Duffy <jasonsduffy@gmail.com>
2013-09-29 05:12:26 -0400

    js/roman-numerals_test fetch

--- javascript/roman-numerals/roman-numerals_test.spec.js
+++ javascript/roman-numerals/roman-numerals_test.spec.js
@@ -6,71 +6,71 @@ describe("toRoman", function() {
     expect(converter.toRoman(1)).toEqual('I');
   });
 
-  it("converts 2", function() {
+  xit("converts 2", function() {
     expect(converter.toRoman(2)).toEqual('II');
   });
 
-  it("converts 3", function() {
+  xit("converts 3", function() {
     expect(converter.toRoman(3)).toEqual('III');
   });
 
-  it("converts 4", function() {
+  xit("converts 4", function() {
     expect(converter.toRoman(4)).toEqual('IV');
   });
 
-  it("converts 5", function() {
+  xit("converts 5", function() {
     expect(converter.toRoman(5)).toEqual('V');
   });
 
-  it("converts 6", function() {
+  xit("converts 6", function() {
     expect(converter.toRoman(6)).toEqual('VI');
   });
 
-  it("converts 9", function() {
+  xit("converts 9", function() {
     expect(converter.toRoman(9)).toEqual('IX');
   });
 
-  it("converts 27", function() {
+  xit("converts 27", function() {
     expect(converter.toRoman(27)).toEqual('XXVII');
   });
 
-  it("converts 48", function() {
+  xit("converts 48", function() {
     expect(converter.toRoman(48)).toEqual('XLVIII');
   });
 
-  it("converts 59", function() {
+  xit("converts 59", function() {
     expect(converter.toRoman(59)).toEqual('LIX');
   });
 
-  it("converts 93", function() {
+  xit("converts 93", function() {
     expect(converter.toRoman(93)).toEqual('XCIII');
   });
 
-  it("converts 141", function() {
+  xit("converts 141", function() {
     expect(converter.toRoman(141)).toEqual('CXLI');
   });
 
-  it("converts 163", function() {
+  xit("converts 163", function() {
     expect(converter.toRoman(163)).toEqual('CLXIII');
   });
 
-  it("converts 402", function() {
+  xit("converts 402", function() {
     expect(converter.toRoman(402)).toEqual('CDII');
   });
 
-  it("converts 575", function() {
+  xit("converts 575", function() {
     expect(converter.toRoman(575)).toEqual('DLXXV');
   });
 
-  it("converts 911", function() {
+  xit("converts 911", function() {
     expect(converter.toRoman(911)).toEqual('CMXI');
   });
 
-  it("converts 1024", function() {
+  xit("converts 1024", function() {
     expect(converter.toRoman(1024)).toEqual('MXXIV');
   });
 
-  it("converts 3000", function() {
+  xit("converts 3000", function() {
     expect(converter.toRoman(3000)).toEqual('MMM');
   });
 
