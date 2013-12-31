400331e214937ffad23270aff8b207f2b454ce6f
Jason Duffy <jasonsduffy@gmail.com>
2013-12-31 13:47:35 -0500

    rb/hamming iteration 5

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,7 @@
 class Hamming
   def self.compute(strand1, strand2)
-    [strand1.length, strand2.length].min.times.count { |i|
+    common_length = [strand1.length, strand2.length].min
+    common_length.times.count { |i|
       strand1[i] != strand2[i]
     }
   end
