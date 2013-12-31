8b8011c362ff79c50e96803bc123d028fe07cd06
Jason Duffy <jasonsduffy@gmail.com>
2013-12-31 10:39:18 -0500

    rb/hamming iteration 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,15 @@
 class Hamming
   def self.compute(strand1, strand2)
     distance = 0
-    if strand1.length > strand2.length
-      strand1 = strand1.slice 0, strand2.length
-    end
-    strand1.split('').each_with_index do |c, i|
+    minimum_length(strand1, strand2).times do |i|
       if strand1[i] != strand2[i]
         distance += 1
       end
     end
     distance
+  end
+
+  def self.minimum_length(strand1, strand2)
+    [strand1.length, strand2.length].min
   end
 end
